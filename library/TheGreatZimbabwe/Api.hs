df4dda352bacb192ddc3f323b4c8bd6d7878cc00
Benjamin Kovach <bkovach13@gmail.com>
2018-11-20 06:58:00 -0500

    Api with database

--- library/TheGreatZimbabwe/Api.hs
+++ library/TheGreatZimbabwe/Api.hs
@@ -1,41 +1,64 @@
-{-# LANGUAGE OverloadedStrings   #-}
-{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE FlexibleContexts      #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedStrings     #-}
+{-# LANGUAGE ScopedTypeVariables   #-}
+{-# LANGUAGE TypeApplications      #-}
+{-# LANGUAGE TypeFamilies          #-}
 module TheGreatZimbabwe.Api where
 
-import           Web.Scotty
-import qualified Web.Scotty                     as Scotty
-
-import           Control.Monad.IO.Class         (liftIO)
+import           Control.Monad.IO.Class
+import           Control.Monad.Logger
+import           Control.Monad.Reader
+import           Control.Monad.Trans.Control
 import           Data.Monoid                    (mconcat)
+import           Data.Pool
+import qualified Data.Text                      as T
+import qualified Data.Text.Lazy                 as TL
 import           Database.Persist.Postgresql
 import qualified Database.Persist.Postgresql    as P
 import           Network.HTTP.Types.Status
+import           Network.Wai                    (Response)
 import           TheGreatZimbabwe.Database.User
 import           TheGreatZimbabwe.Error
 import           TheGreatZimbabwe.NewGame
 import           TheGreatZimbabwe.Types
+import           Web.Scotty
+import qualified Web.Scotty                     as Scotty
+
+devString :: ConnectionString
+devString = ""
 
 main :: IO ()
-main = scotty 3000 $ do
-  -- create a new game
-  post "/new-game" $ do
-    userIds :: [UserId] <- jsonData
-    -- TODO: pass a connection pool here
-    users               <- runDB $ selectList [UserId <-. userIds]
-    let playerInfos = map toPlayerInfoWithId users
-    eGameData <- getGameEvent <$> liftIO (newGame playerInfos)
-    case eGameData of
-      Left gameError -> case gameError of
-        InvalidAction err -> do
-          status forbidden403 *> json gameError
-        InternalError err -> status internalServerError500 *> json gameError
-      Right gameData -> do
-        mSavedGameData <- runDB $ insert gameData >>= P.get
-        case mSavedGameData of
-          Nothing            -> raise "Game was not created"
-          Just savedGameData -> json savedGameData
+main = do
+  runStdoutLoggingT $ withPostgresqlPool devString 2 $ \pool -> do
+    -- TODO: obviously no logging will happen here because we're just lifting.
+    -- We probably want that to not be the case.
+    liftIO $ scotty 3000 $ do
+      -- create a new game
+      post "/new-game" $ do
+        userIds :: [UserId] <- map (toSqlKey . fromIntegral)
+          <$> param @[Int] "userIds"
+        users <- runDB pool $ selectList [UserId <-. userIds] []
+        let playerInfos = map toPlayerInfoWithId users
+        eGameData <- getGameEvent <$> liftIO (newGame playerInfos)
+        case eGameData of
+          Left gameError -> case gameError of
+            InvalidAction err -> do
+              status forbidden403 *> json gameError
+            InternalError err ->
+              status internalServerError500 *> json gameError
+          Right gameData -> do
+            --mSavedGameData <- runDB pool $ do
+              --key <- insert gameData
+              --P.getEntity key
+            mSavedGameData <- pure (Just ())
+            case mSavedGameData of
+              Nothing            -> status internalServerError500 *> json ()
+              Just savedGameData -> json savedGameData
 
-  -- here for testing, for now
-  Scotty.get "/:word" $ do
-    beam <- param "word"
-    html $ mconcat ["<h1>Scotty, ", beam, " me up!</h1>"]
+runDB
+  :: (MonadIO m, MonadBaseControl IO m)
+  => ConnectionPool
+  -> SqlPersistT m a
+  -> m a
+runDB pool act = withResource pool (runReaderT act)
--- library/TheGreatZimbabwe/Error.hs
+++ library/TheGreatZimbabwe/Error.hs
@@ -1,14 +1,25 @@
+{-# LANGUAGE DeriveGeneric #-}
 module TheGreatZimbabwe.Error where
 
-import qualified Data.Text as T
+import           Data.Aeson
+import qualified Data.Aeson   as Aeson
+import qualified Data.Text    as T
+import           GHC.Generics
 
 data GameError
   = InvalidAction T.Text
   | InternalError T.Text
-  deriving (Show, Eq)
+  deriving (Show, Eq, Generic)
 
 invalidAction :: T.Text -> Either GameError a
 invalidAction = Left . InvalidAction
 
 internalError :: T.Text -> Either GameError a
 internalError = Left . InternalError
+
+instance ToJSON GameError where
+  toJSON = genericToJSON defaultOptions
+  toEncoding = genericToEncoding defaultOptions
+
+instance FromJSON GameError where
+  parseJSON = genericParseJSON defaultOptions
