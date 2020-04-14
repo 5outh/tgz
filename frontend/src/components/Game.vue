7c33fc09d3b452bbea26716c215fa02a0e111a95
Benjamin Kovach <bkovach13@gmail.com>
2020-04-13 20:26:42 -0400

    WIP game parsing

--- frontend/src/components/Game.vue
+++ frontend/src/components/Game.vue
@@ -9,13 +9,18 @@
 import { getAuthToken } from "@/auth";
 import { Component, Vue } from "vue-property-decorator";
 import { authorizedFetch } from "@/api";
-import type { Game } from "@/types";
+import type { GameView } from "@/types"
+import { GameViewV } from "@/types";
+import { pipe } from 'fp-ts/lib/pipeable'
+import { fold } from 'fp-ts/lib/Either'
+import { PathReporter } from 'io-ts/lib/PathReporter'
+import * as t from 'io-ts'
 
 @Component
 export default class Home extends Vue {
   private token: string | null = null;
   private usernameText: string = "";
-  private game: Game | null = null;
+  private game: GameView | null = null;
 
   mounted() {
     const self = this;
@@ -27,8 +32,12 @@ export default class Home extends Vue {
     this.token = token;
 
     this.fetchGame(parseInt(this.$route.params.id), token)
-      .then((game: Game) => {
-        self.game = game;
+      .then((game: GameView | void) => {
+        if (game) {
+          self.game = game;
+        } else {
+          throw new Error("Game was not parseable")
+        }
       })
       .catch(console.error);
   }
@@ -36,7 +45,6 @@ export default class Home extends Vue {
   private createNewGame() {
     const users = this.usernameText.split("\n");
     if (this.token) {
-      console.log(users);
       this.newGame(users, this.token).then(console.log);
     }
   }
@@ -45,7 +53,7 @@ export default class Home extends Vue {
     alert("usernames");
   }
 
-  private newGame(usernames: Array<string>, token: string): Promise<Game> {
+  private newGame(usernames: Array<string>, token: string): Promise<GameView> {
     return authorizedFetch(`http://localhost:8000/games`, token, {
       method: "POST",
       body: JSON.stringify({ usernames, name: "TODO" })
@@ -57,15 +65,24 @@ export default class Home extends Vue {
     });
   }
 
-  private fetchGame(gameId: number, token: string): Promise<Game> {
+  private fetchGame(gameId: number, token: string): Promise<GameView | void> {
+    const onLeft = (errors : t.Errors) => {}
+    const onRight = (game : GameView) => game
+
     return authorizedFetch(`http://localhost:8000/games/${gameId}`, token).then(
       (response: Response) => {
         if (!response.ok) {
           throw new Error(`HTTP error! status: ${response.status}`);
         }
-        return response.json();
-      }
-    );
+        return response.json()
+      }).then((data: object) => {
+        console.log({data})
+        const result = GameViewV.decode(data)
+
+        console.log(PathReporter.report(result))
+
+        return pipe(result, fold(onLeft, onRight));
+      });
   }
 }
 </script>
--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -2,33 +2,27 @@ import * as t from 'io-ts'
 
 const Username = t.type({
   username: t.string
-})
+}, "Username")
 export type Username = t.TypeOf<typeof Username>
 
 const PlayerInfo = t.type(
   { username : Username
   , email    : t.string
   , playerId : t.number
-  })
+  }, "PlayerInfo")
 export type PlayerInfo = t.TypeOf<typeof PlayerInfo>
 
 const Points = t.type(
   { points : t.number
   , step : t.number
-  })
+  }, "Points")
 export type Points = t.TypeOf<typeof Points>
 
-const Rotated = <C extends t.Mixed>(codec: C) => t.union([t.type({Rotated: codec}), t.type({UnRotated: codec}) ])
-
-// const _Rotated = <C extends t.Mixed>(codec: C) => t.type({Rotated: codec})
-// const _UnRotated = <C extends t.Mixed>(codec: C) => t.type({UnRotated: codec})
-// const Rotated = <C extends t.Mixed>(codec: C) => t.union([_Rotated<C>,_UnRotated<C>])
-// export type Rotated<T> = t.TypeOf<typeof Rotated<T>>
-
+const Rotated = <C extends t.Mixed>(codec: C) => t.union([t.type({Rotated: codec}), t.type({UnRotated: codec}) ], "Rotated")
 const TechnologyCardState = t.type(
   { price : t.number
   , cattle : t.number
-  })
+  }, "TechnologyCardState")
 export type TechnologyCardState = t.TypeOf<typeof TechnologyCardState>
 
 const Craftsman = t.keyof({
@@ -39,7 +33,7 @@ const Craftsman = t.keyof({
   vesselMaker: null,
   throneMaker: null,
   sculptor: null
-})
+}, "Craftsman")
 export type Craftsman = t.TypeOf<typeof Craftsman>
 
 const TechnologyCard = t.type(
@@ -48,15 +42,14 @@ const TechnologyCard = t.type(
   , victoryRequirement : t.number
   , victoryPoints      : t.number
   , cost               : t.number
-  })
+  }, "TechnologyCard")
 export type TechnologyCard = t.TypeOf<typeof TechnologyCard>
 
-const Activation = t.union([t.literal('BuilderActive'), t.literal('NomadsActive'), t.literal('None')])
+const Activation = t.union([t.literal('BuilderActive'), t.literal('NomadsActive'), t.literal('None')], "Activation")
 export type Activation = t.TypeOf<typeof Activation>
 
-const Location = t.type({x: t.number, y: t.string})
+const Location = t.type({x: t.number, y: t.string}, "Location")
 export type Location = t.TypeOf<typeof Location>
-
 
 const Empire = t.union([
   t.literal("kilwa")
@@ -64,7 +57,7 @@ const Empire = t.union([
   , t.literal("zulu")
   , t.literal("lozi")
   , t.literal("mapungubwe")
-  ])
+  ], "Empire")
 export type Empire = t.TypeOf<typeof Empire>
 
 const Specialist = t.keyof({
@@ -73,7 +66,7 @@ const Specialist = t.keyof({
   Herd: null,
   Builder: null,
   Nomads: null
-  })
+  }, "Specialist")
 export type Specialist = t.TypeOf<typeof Specialist>
 
 const God = t.keyof({
@@ -89,9 +82,8 @@ const God = t.keyof({
   Qamata: t.number ,
   Engai : t.any ,
   Xango  : t.any ,
-})
+}, "God")
 export type God = t.TypeOf<typeof God>
-
 
 const Player = t.type(
   { info               : t.union([PlayerInfo,  t.null])
@@ -105,10 +97,10 @@ const Player = t.type(
   , specialists        : t.array(t.record(Specialist, t.number))
   , god                : t.union([God , t.null])
   , activations        : t.array(Activation)
-  })
+  }, "Player")
 export type Player = t.TypeOf<typeof Player>
 
-const EmpirePlaque = t.union([t.type({PlayerPlaque: Empire}), t.type({ShadipinyiPlaque: t.any})])
+const EmpirePlaque = t.union([t.type({PlayerPlaque: Empire}), t.type({ShadipinyiPlaque: t.any})], "EmpirePlaque")
 export type EmpirePlaque = t.TypeOf<typeof EmpirePlaque>
 
 const GenerosityOfKingsState = t.type(
@@ -116,7 +108,7 @@ const GenerosityOfKingsState = t.type(
   , cattlePool    : t.number
   , lastBid       : t.union([t.number , t.null])
   , playersPassed : t.array( t.number )
-  })
+  }, "GenerosityOfKingsState")
 export type GenerosityOfKingsState = t.TypeOf<typeof GenerosityOfKingsState>
 
 const Phase = t.keyof({
@@ -126,49 +118,56 @@ const Phase = t.keyof({
   "ReligionAndCulture": null,
   "Revenues": null,
   "LetUsCompareMythologies": null
-})
+}, "Phase")
 export type Phase = t.TypeOf<typeof Phase>
 
 const Round = t.type(
   { players                : t.array( t.number )
-  , currentPlayer          : t.union([t.number , t.null])
-  , usedMarkers            : t.array(t.record(Location, t.number))
-  , generosityOfKingsState : GenerosityOfKingsState
-  , currentPhase           : t.union([Phase , t.null])
+  , current_player          : t.union([t.number , t.null])
+  , used_markers            : t.array(t.record(Location, t.number))
+  , generosity_of_kings_state : GenerosityOfKingsState
+  , current_phase           : t.union([Phase , t.null])
   , step : t.number
-  })
+  }, "Round")
 export type Round = t.TypeOf<typeof Round>
 
-const Resource = t.keyof({ Clay: null, Wood: null , Ivory: null, Diamonds: null })
+const Resource = t.keyof({ Clay: null, Wood: null , Ivory: null, Diamonds: null }, "Resource")
 export type Resource = t.TypeOf<typeof Resource>
 
 const Land = t.keyof({
     StartingArea : t.any,
     BlankLand : t.any,
     Resource : Resource
-  })
+  }, "Land")
 export type Land = t.TypeOf<typeof Land>
 
 const Square = t.keyof({
   Water: t.any,
   Land: Land
-})
+}, "Square")
 export type Square = t.TypeOf<typeof Square>
 
-const MapLayout = t.type({ mapLayout : t.array(t.record(Location, Square)) })
+  const MapLayout = t.type({ mapLayout : t.array(t.record(Location, Square)) }, "MapLayout")
 export type MapLayout = t.TypeOf<typeof MapLayout>
 
-const Game = t.type(
+export const GameV = t.type(
   { players   : t.array(t.record(t.number, Player))
   , round     : Round
   , mapLayout : MapLayout
-  , technologyCards : t.array(t.record(Craftsman, t.array(TechnologyCard)))
+  , technology_cards : t.array(t.record(Craftsman, t.array(TechnologyCard)))
   , gods : t.array( God )
   , specialists : t.array( Specialist )
   , winner    : t.union([t.number , t.null])
   , step : t.number
-  , resourceTiles : t.array(t.record(Resource, t.number))
-  , waterTiles : t.number
-  , craftsmanTiles : t.array(t.record(Craftsman, t.number))
-  })
-export type Game = t.TypeOf<typeof Game>
+  , resource_tiles : t.array(t.record(Resource, t.number))
+  , water_tiles : t.number
+  , craftsman_tiles : t.array(t.record(Craftsman, t.number))
+  }, "Game")
+export type Game = t.TypeOf<typeof GameV>
+
+export const GameViewV = t.type({
+  id: t.number,
+  name: t.string,
+  state: GameV,
+}, "GameView")
+export type GameView = t.TypeOf<typeof GameViewV>
